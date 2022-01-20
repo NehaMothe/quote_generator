7cf59c9b74f6314dcf769a4ce21503ada4885114
Neha Mothe <nehamothe03@gmail.com>
2022-01-20 17:16:06 +0530

    first Commit

---
+++ script.js
@@ -0,0 +1,50 @@
+let quoteContainer = document.getElementById('Quote-container');
+let quoteText = document.getElementById('quoteText');
+let authorText = document.getElementById('auhtorName');
+let twitterBtn = document.getElementById('twitter-button');
+let newQuoteBtn = document.getElementById('new-quote');
+//let apiQuotes = [];
+
+function newQuote() {
+    //pick  a random quote
+    let quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+    //if author if unknown
+    if (quote.author == null) {
+        authorText.textContent == 'Unknown';
+    }
+    else {
+        authorText.textContent = quote.author;
+    }
+    //quote length for fint size
+    if (quote.text.length > 100) {
+        quoteText.classList.add('long-quote');
+    }
+    else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
+}
+function shareQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+
+//event listner
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", shareQuote);
+
+// Get quotes from API
+// async function getQuotes() {
+//     const apiURL = 'https://type.fit/api/quotes';
+//     try {
+//         const response = await fetch(apiURL);
+//         apiQuotes = await response.json();
+//         newQuote();
+//     } catch (error) {
+//         //error handled
+//     }
+// };
+// on load
+//getQuotes();
+newQuote();
